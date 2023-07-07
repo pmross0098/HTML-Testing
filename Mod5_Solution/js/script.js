67b200f1659bd8fc1b8310994bc5c108ba94bb4f
Marty Ross <pmross0098@users.noreply.github.com>
2023-07-07 14:34:08 -0700

    Update script.js
    Fixed a bug

--- Mod5_Solution/js/script.js
+++ Mod5_Solution/js/script.js
@@ -104,7 +104,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-      console.log(chosenCategoryShortName);
+      // console.log(chosenCategoryShortName);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -117,8 +117,10 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", "'" + chosenCategoryShortName + "'")
+      // console.log("'" + chosenCategoryShortName + "'")
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
 
+      // console.log(homeHtml)
 // Must run Chrome allowing for file tree access from command line:
 // "C:\Program Files\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files
 
